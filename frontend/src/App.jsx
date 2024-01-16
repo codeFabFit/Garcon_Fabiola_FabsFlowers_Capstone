f488ec72f774fd5bd2716180f83df415acedfde1
codeFabFit <fabgar14@gmail.com>
2024-01-16 11:40:25 -0500

    starting with frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <header>
+        <a href="/">FabsFlowers</a>
+
+      </header>
+    </div>
+   
+    </>
+  )
+}
+
+export default App
