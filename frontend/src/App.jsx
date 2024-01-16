5e9dbeda946d010952ddc6c6448f9e24c19b4876
codeFabFit <fabgar14@gmail.com>
2024-01-16 12:24:42 -0500

    added flower data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
 
@@ -10,7 +10,9 @@ function App() {
     <div>
       <header>
         <a href="/">FabsFlowers</a>
-
+          <main>
+            List Products
+          </main>
       </header>
     </div>
    
