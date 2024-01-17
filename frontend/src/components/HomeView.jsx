2c2143166d42b88d495a8c79f26ca5bcfcf455f7
codeFabFit <fabgar14@gmail.com>
2024-01-17 13:21:32 -0500

    trouble fetching backend data

--- frontend/src/components/HomeView.jsx
+++ frontend/src/components/HomeView.jsx
@@ -1,23 +1,40 @@
 // this is the home screen view
 
 // import React from 'react'
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
-import data from "../data"
+// import data from "../data"
+// once you have added setProducts from useState
+// you can remove the imported data cause its reading from back end
+import axios from "axios"
 
 function HomeView() {
+  const [products, setProducts] = useState([])
+// using usestate
+
+
+// useEffect 
+  useEffect(()=> {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data)
+    };
+    fetchData();
+  }, []);
+
+
   return (
     <div>
       <h3>Featured Flowers</h3>
 {/* add the homeview information from app.jsx */}
 <div className="products">
-            {
-              data.products.map(product => 
-                  (<div className="product" key={product.slug}>
-                    <Link to={`/product/${product.id}`}>
+            {Array.isArray(products) && products.map((product) => (
+                  <div className="product" key={product.slug}>
+                    <Link to={`/product/${product.slug}`}>
                 <img src={product.image} alt={product.name} />
                 </Link>
                 <div className="prod-info">
-                <Link to={`/products/${product.id}`}>
+                <Link to={`/products/${product.slug}`}>
 {/* added a link to make it so you click the name it will bring you to that page info */}
                 <p>{product.name}</p>
                 </Link>
