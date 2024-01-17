5439c900068afa20132d7d0d5c1fc39c62dbfa31
codeFabFit <fabgar14@gmail.com>
2024-01-17 15:56:18 -0500

    added useReducer

--- frontend/src/components/HomeView.jsx
+++ frontend/src/components/HomeView.jsx
@@ -1,29 +1,60 @@
 // this is the home screen view
 
 // import React from 'react'
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 // import data from "../data";
 // once you have added setProducts from useState
 // you can remove the imported data cause its reading from back end
 import axios from "axios";
 
+ 
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'FETCH_REQUEST':
+      return {...state, loading: true}
+      case 'FETCH_SUCCESS':
+      return {...state, products: action.payload, loading: false}
+      case 'FETCH_FAIL':
+      return {...state, loading: false, error: action.payload}
+      default:
+        return state;
+  }
+}
+
+
 function HomeView() {
-  const [products, setProducts] = useState([]);
+
+// adding useReducer and getting rid of useState
+  const [{loading, error, products}, dispatch] = useReducer(reducer, {
+    products: [],
+    loading: true,
+    error: '',
+  })
+  // const [products, setProducts] = useState([]);
   // using usestate
 
   // useEffect
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("http://localhost:5000/api/products");
-      setProducts(result.data);
-      console.log(result)
+      // adding dispatch
+      dispatch({type: 'FETCH_REQUEST'})
+      try {
+        const result = await axios.get("http://localhost:5000/api/products");
+        dispatch({type: 'FETCH_SUCCESS', payload: result.data})
+
+      } catch (error) {
+        dispatch({type: 'FECTH_FAIL', payload: error.message})
+      }
+      
+      // setProducts(result.data);
+      // console.log(result)
     };
     fetchData();
   }, []);
 
   const isLoaded =   (  <>
-        <h3>Featured Flowers</h3>;
+        <h3>Featured Flowers</h3>
         {/* add the homeview information from app.jsx */}
         <div className="products">
           {
