6485a2285ad3fd46376f39699f9984040ee019d6
codeFabFit <fabgar14@gmail.com>
2024-01-17 23:47:33 -0500

    trouble rendering with react-bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,31 +4,47 @@
 // import './App.css'
 
 // import data from "./data"
-import {BrowserRouter, Route, Link, Routes} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import HomeView from "./components/HomeView"
 import ProductView from './components/ProductView'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import {LinkContainer} from 'react-router-bootstrap'
+
 
 function App() {
 
   return (
     
     <BrowserRouter>
-    <div>
+    <div className='d-flex flex-colum site-container'>
       <header>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+            <Navbar.Brand>FabsFlowers</Navbar.Brand>
+            </LinkContainer>
+          </Container>
+        </Navbar>
         {/* you want to change from a href to link stop page refresh */}
-        <Link to="/">FabsFlowers</Link>
+       {/* we are removing this one below bc we are using bootstrap */}
+        {/* <Link to="/">FabsFlowers</Link> */}
        </header>  
           <main>
 
             {/* add routing here */}
-      <Routes>
-        <Route path="/products/:slug" element={<ProductView />}></Route>
-        <Route path="/" element={<HomeView />}></Route>
-
-        
-      </Routes>
-
+            <Container>
+                  <Routes>
+                  <Route path="/products/:slug" element={<ProductView />}></Route>
+                  <Route path="/" element={<HomeView />}></Route>
+                </Routes>
+          </Container>
       </main>
+      <footer>
+        <div className='text-center'>
+          All Rights Reserved @ Fabiola Garcon
+        </div>
+      </footer>
        </div>    
    </BrowserRouter>
     
