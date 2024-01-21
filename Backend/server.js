04ad35cd01acfbddc71d94762f3c9f6fc4b21512
codeFabFit <fabgar14@gmail.com>
2024-01-21 16:08:12 -0500

    created and connected to mongodb

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,20 @@
 import express from 'express';
 import data from './data.js';
 import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+// connecting mongoose with mongodb 
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('connected to db')
+})
+.catch((err) => {
+    console.log(err.message)
+})
 
 const app = express();
 app.use(cors())
